4ae02fa8af035c0a425eb70fdb63b6027283d856
brandon <zkbrndn@yahoo.com>
2017-01-18 15:44:47 -0600

    Updating modules and files

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-console.log('Chatly started!');
+http.listen(80, function(){
+  console.log('listening on *:80');
+});
